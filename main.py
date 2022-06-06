2046c23356734223aaa8c11db1e2399d2be57630
ValenTiss <100385088+ValenTiss@users.noreply.github.com>
2022-06-06 17:14:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #Para generar primeros centroides
 import random 
-from base.py import Generador_Datos
+from base import Generador_Datos
+from sklearn.cluster import KMeans
+from Clase_membresia import Membresia_de_cluster
+from Clase_centroides  import Centroides
 
 def calcular_indices_random(X):
   return random.randint(0, len(X-1))
@@ -10,10 +13,6 @@ generador_datos = Generador_Datos()
 (t, X) = generador_datos.generar_datos(numberSamplesPerClass = 200, mean1 = [2, 3], mean2 = [6, 6], stds1 = [3, 3], stds2 = [4, 3])
 kmeans = KMeans(n_clusters=2, random_state=0)
 kmeans.fit(X)
-
-
-
-
 
 #Se calculan los primeros centroides
 membresia= Membresia_de_cluster()
@@ -32,7 +31,7 @@ centroide2= Centroides(X[numero_aleatorio2][0], X[numero_aleatorio2][1])
 membresia.agregar_centroides([centroide1,centroide2])
 P = 3 #cantidad de iteraciones  ??? cambiar a input y verificar que sea numero
 for i in range(P):
-  pareja_centorides =  membresia.getLista_Centroides()[i]
+  pareja_centroides =  membresia.getLista_centroides()[i]
   W = membresia.calcular_w( pareja_centroides[0], pareja_centroides[1], X)
   #Se genera la lista de matriz_de_pertenecientes de cada centroide
   pareja_centroides[0].agregregar_a_matriz( W, X, 0)
