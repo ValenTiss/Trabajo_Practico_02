800c560ca9a06adb5f77415baf6af091e030dce9
ValenTiss <100385088+ValenTiss@users.noreply.github.com>
2022-06-08 01:40:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #Para generar primeros centroides
 import random 
 from base import Generador_Datos
@@ -6,10 +7,22 @@ from Clase_membresia import Membresia_de_cluster
 from Clase_centroides  import Centroides
 from Clase_graficador import Graficador
 import numpy as np
+=======
+probador=Pruebas()
+numero_de_prueba= input("Si desea una prueba rápida de las pruebas puestas en el documento presione 1,2,3 o 4. \n En caso contrario, si desea digitar manualmente los datos presione cualquier otro número")
+try:
+  numero_de_prueba=int(numero_de_prueba)
+except:
+  raise ValueError("La opción debe tiene que ser número un número entero")
+>>>>>>> 3a174c81f07c5639015eaadf7ee0f6ec15e183e6
 
-def calcular_indices_random(X):
-  return random.randint(0, len(X)-1 )
+numberSamplesPerClass= input("Ingese la cantidad de datos que desea crear: ")
+try:
+  numberSamplesPerClass=int(numberSamplesPerClass)
+except:
+  raise ValueError("La cantidad de datos debe ser un número")
 
+<<<<<<< HEAD
 
 
 #Se generan datos, esto es del profe
@@ -72,3 +85,37 @@ for i in range(P):
   #Se crean los nuevos centroides 
   membresia.agregar_centroides([ membresia.calcular_nuevo_centroide1(W,X) , membresia.calcular_nuevo_centroide2(W,X)])
   
+=======
+if numberSamplesPerClass > 0:
+  if numero_de_prueba == 1:
+    probador.prueba1()
+  elif numero_de_prueba == 2:
+    probador.prueba2()
+  elif numero_de_prueba == 3:
+    probador.prueba3()
+  elif numero_de_prueba == 4:
+    probador.prueba4()
+  else:
+    mean1_x= input("Ingese el dato mean1_x: ")
+    mean1_y= input("Ingese el dato mean1_y: ")
+    mean2_x= input("Ingese el dato mean2_x: ")
+    mean2_y= input("Ingese el dato mean2_y: ")
+    stds1_x= input("Ingese el dato stds1_x: ")
+    stds1_y= input("Ingese el dato stds1_y: ")
+    stds2_x= input("Ingese el dato stds2_x: ")
+    stds2_y= input("Ingese el dato stds2_y: ")
+    try:
+      mean1_x= int(mean1_x)
+      mean1_y= int(mean1_y)
+      mean2_x= int(mean2_x)
+      mean2_y= int(mean2_y)
+      stds1_x= int(stds1_x)
+      stds1_y= int(stds1_y)
+      stds2_x= int(stds2_x)
+      stds2_y= int(stds2_y)
+    except:
+      raise ValueError("Tipo de dato incorrecto, datos introducidos deben ser números")
+  probador.prueba(numberSamplesPerClass, [mean1_x, mean1_y], [mean2_x,mean2_y], [stds1_x,stds1_y], [stds2_x,stds2_y])
+else:
+    raise ValueError("La cantidad de datos debe ser un número natural")
+>>>>>>> 3a174c81f07c5639015eaadf7ee0f6ec15e183e6
