d0cd5b182ed953f3a54a483c5dfa209f5218a45f
FedeAlfaro <71413273+FedeAlfaro@users.noreply.github.com>
2022-06-07 23:57:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -22,22 +22,22 @@ except:
 
 if numberSamplesPerClass > 0:
   if numero_de_prueba == 1:
-    probador.prueba1()
+    probador.prueba1(numberSamplesPerClass)
   elif numero_de_prueba == 2:
-    probador.prueba2()
+    probador.prueba2(numberSamplesPerClass)
   elif numero_de_prueba == 3:
-    probador.prueba3()
+    probador.prueba3(numberSamplesPerClass)
   elif numero_de_prueba == 4:
-    probador.prueba4()
+    probador.prueba4(numberSamplesPerClass)
   else:
-    mean1_x= input("Ingese la cantidad de datos que desea crear: ")
-    mean1_y= input("Ingese la cantidad de datos que desea crear: ")
-    mean2_x= input("Ingese la cantidad de datos que desea crear: ")
-    mean2_y= input("Ingese la cantidad de datos que desea crear: ")
-    stds1_x= input("Ingese la cantidad de datos que desea crear: ")
-    stds1_y= input("Ingese la cantidad de datos que desea crear: ")
-    stds2_x= input("Ingese la cantidad de datos que desea crear: ")
-    stds2_y= input("Ingese la cantidad de datos que desea crear: ")
+    mean1_x= input("Ingese el dato mean1_x: ")
+    mean1_y= input("Ingese el dato mean1_y: ")
+    mean2_x= input("Ingese el dato mean2_x: ")
+    mean2_y= input("Ingese el dato mean2_y: ")
+    stds1_x= input("Ingese el dato stds1_x: ")
+    stds1_y= input("Ingese el dato stds1_y: ")
+    stds2_x= input("Ingese el dato stds2_x: ")
+    stds2_y= input("Ingese el dato stds2_y: ")
     try:
       mean1_x= int(mean1_x)
       mean1_y= int(mean1_y)
